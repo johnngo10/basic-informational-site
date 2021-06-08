1330c377b6c244503b6e7e6695f4efb482338fca
xuanngo10 <xuan.ngo10@gmail.com>
2021-06-08 15:03:43 -0700

    display 404 page for urls that doesn't exist

---
+++ README.md
@@ -0,0 +1,14 @@
+# Basic Informational Page
+
+A very basic website that was built with Node.js
+
+- [Live Demo](http://xuanngo.com/mint-signup-page/)
+
+## Built With
+
+- HTML
+- Node.js
+
+## License
+
+This project is licensed under MIT
--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+const errorPage = fs.readFileSync("404.html", "utf-8", (err, data) => {
+  if (err) throw err;
+  return data;
+});
+
 http
   .createServer(function (req, res) {
     const q = url.parse(req.url, true);
-    const filename =
+    let filename =
       q.pathname === "/" ? "./index.html" : "." + q.pathname + ".html";
     fs.readFile(filename, function (err, data) {
       if (err) {
         res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 NOT FOUND");
+        res.write(errorPage);
+        return res.end();
       }
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
