59e8427757d7482a36dc867827a5f03327f9c430
xuanngo10 <xuan.ngo10@gmail.com>
2021-06-08 11:21:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+http
+  .createServer(function (req, res) {
+    const q = url.parse(req.url, true);
+    const filename =
+      q.pathname === "/" ? "./index.html" : "." + q.pathname + ".html";
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 NOT FOUND");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
